ea621a28285f1f8211b614864f0bbb77e32b1d44
yijia <sixerwyj@gmail.com>
2020-03-06 14:41:41 -0500

    modified one with true visual odometry

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 robot_pose_ekf [![Build Status](https://travis-ci.com/ros-planning/robot_pose_ekf.svg?branch=master)](https://travis-ci.org/ros-planning/robot_pose_ekf)
 ========================================================================================================================================================
+
+## Combining STag and IMU with EKF
+
+### Dependency
+1. stag package
+
+2. imu_complementary_filter package
+
+3. ar_track_alvar package
+
--- scripts/ekf.py
+++ scripts/ekf.py
@@ -12,20 +12,9 @@ from mpl_toolkits.mplot3d import Axes3D
 from pynput import keyboard
 import math
 
-
 RATE = 30
-IMU_SIGMA = 0.01
+IMU_SIGMA = 1e-4
 VO_SIGMA = 0.01
-
-class STag:
-    def __init__(self, tagid, x, y, z, roll, pitch, yaw):
-        self.id = tagid
-        self.x = x
-        self.y = y
-        self.z = z
-        self.roll = roll
-        self.pitch = pitch
-        self.yaw = yaw
 
 class VO:
     def __init__(self):
@@ -33,8 +22,10 @@ class VO:
         tag_sub = rospy.Subscriber("bluerov_controller/ar_tag_detector", AlvarMarkers, self.tag_callback)
         path_sub = rospy.Subscriber("robot_pose_ekf/odom_combined", PoseWithCovarianceStamped, self.path_callback)
         self.posepub = rospy.Publisher('pose_vo', Odometry, queue_size = 0)
-        #imusub = rospy.Subscriber('imu/data', Imu, self.imu_callback)
-        #self.imupub = rospy.Publisher('imu_data', Imu, queue_size=0)
+        imusub = rospy.Subscriber('imu/data', Imu, self.imu_callback)
+        imu_raw_sub = rospy.Subscriber('mavros/imu/data_raw', Imu, self.imu_raw_callback)
+        self.imupub = rospy.Publisher('imu/data_raw', Imu, queue_size=0)
+        self.imu_modified_pub = rospy.Publisher('imu/modified_data', Imu, queue_size=0)
 
         self.listener = tf.TransformListener()
         self.broadcaster = tf.TransformBroadcaster()
@@ -45,9 +36,10 @@ class VO:
 
         self.init_trans = []
         self.init_rot = []
+        self.combine_init_trans = []
         self.pose_is_init = False
 
-        self.init_time = rospy.Time.now().to_sec()
+        self.init_time = rospy.Time.now().secs
         self.vo_init_time = 0
         self.t_vo = []
         self.t = []
@@ -72,7 +64,6 @@ class VO:
         self.init_imu = False
         self.last_imu_time = 0
 
-        #self.init_time = 0
         self.get_init_time = False
         self.start_vo = False
 
@@ -106,71 +97,59 @@ class VO:
             self.pitch.append(euler[1])
             self.yaw.append(euler[2])
 
+    def imu_raw_callback(self, msg):
+        if self.start_vo:
+            imu_msg = Imu()
+            imu_msg.header = msg.header
+            imu_msg.angular_velocity = msg.angular_velocity
+            imu_msg.angular_velocity_covariance = msg.angular_velocity_covariance
+            imu_msg.linear_acceleration = msg.linear_acceleration
+            imu_msg.linear_acceleration_covariance = msg.linear_acceleration_covariance
+            imu_msg.header.frame_id = "imu"
+            for i in range(0, 9, 4):
+                imu_msg.orientation_covariance[i] = IMU_SIGMA**2
+            self.imupub.publish(imu_msg)
+
     def imu_callback(self, msg):
         if self.start_vo:
             ori = msg.orientation
             orientation = [ori.x, ori.y, ori.z, ori.w]
             euler = tf.transformations.euler_from_quaternion(orientation)
             self.t_imu.append(rospy.Time.now().to_sec() - self.vo_init_time)
-            self.roll_imu.append(euler[0])
+            self.roll_imu.append(euler[0]+1.62)
             self.pitch_imu.append(euler[1])
             self.yaw_imu.append(euler[2])
+            #print(msg.angular_velocity.x, euler[0])
 
-        # if not self.init_imu:
-        #     self.last_imu_time = msg.header.stamp.to_sec()
-        #     self.init_imu = True
-        #     return
-
-        # imu_msg = Imu()
-        # imu_msg.header = msg.header
-        # imu_msg.header.frame_id = "imu"
-        # dt = (msg.header.stamp.nsecs - self.last_imu_time)*1e-9
-        # self.last_imu_time = msg.header.stamp.nsecs
-
-        # ang_v = msg.angular_velocity
-        # q_rot = tf.transformations.quaternion_from_euler(ang_v.x, ang_v.y, ang_v.z)
-        # ori = msg.orientation
-        # q_ori = [ori.x, ori.y, ori.z, ori.w]
-        # q_d = tf.transformations.quaternion_multiply(q_ori, q_rot)
-
-        # imu_msg.orientation.x = q_d[0]*dt*0.5
-        # imu_msg.orientation.y = q_d[1]*dt*0.5
-        # imu_msg.orientation.z = q_d[2]*dt*0.5
-        # imu_msg.orientation.w = q_d[3]*dt*0.5
-
-        # for i in range(0, 9, 4):
-        #     imu_msg.orientation_covariance[i] = IMU_SIGMA**2
-
-        #self.imupub.publish(imu_msg)
-
+            ori = tf.transformations.quaternion_from_euler(euler[0]+1.62, euler[1], euler[2])
+            imu_msg = Imu()
+            imu_msg.header = msg.header
+            imu_msg.angular_velocity = msg.angular_velocity
+            imu_msg.angular_velocity_covariance = msg.angular_velocity_covariance
+            imu_msg.linear_acceleration = msg.linear_acceleration
+            imu_msg.linear_acceleration_covariance = msg.linear_acceleration_covariance
+            imu_msg.orientation_covariance = msg.orientation_covariance
+            imu_msg.orientation.x = ori[0]
+            imu_msg.orientation.y = ori[1]
+            imu_msg.orientation.z = ori[2]
+            imu_msg.orientation.w = ori[3]
+            self.imu_modified_pub.publish(imu_msg)
 
     def tag_callback(self, msg):
         if len(msg.markers) != 0:
             print("detect marker")
             self.tag_exist = True
             marker = msg.markers[0]
-            # tagid = marker.id
-            # pose = marker.pose.pose.position
-            # x = pose.x
-            # y = pose.y
-            # z = pose.z
-            # orientation = marker.pose.pose.orientation
-            # orientation_list = [orientation.x, orientation.y, orientation.z, orientation.w]
-            # (roll, pitch, yaw) = euler_from_quaternion(orientation_list)
-            #print(self.markers)
-        
-            #vo_msg = Odometry()
-            #self.posepub.publish(vo_msg)
 
             if not self.get_init_time:
                 self.init_time = marker.header.stamp.secs
                 self.get_init_time = True
                 print("get init time", self.init_time)
 
-            duration = marker.header.stamp.to_sec() - self.init_time
+            duration = marker.header.stamp.secs - self.init_time
             print("duration:"+str(duration)+"s")
             # the tag is shaking in the first few seconds
-            if duration > 8 and not self.start_vo:
+            if not self.start_vo:
                 self.start_vo = True
                 self.vo_init_time = rospy.Time.now().to_sec()
             if duration > 22:
@@ -179,14 +158,11 @@ class VO:
         else: self.tag_exist = False
 
     def relative_transformation(self, trans, rot):
-        T1 = self.transformation_matrix(trans, self.quat2R(rot))
-        T0 = self.transformation_matrix(self.init_trans, self.quat2R(self.init_rot))
+        for i in range(3):
+            trans[i] = trans[i] - self.init_trans[i]
 
-        T = np.matmul(T0, T1)
-        R = T[0:3, 0:3]
-        
-        trans = [T[0,3], T[1,3], T[2,3]]
-        rot = self.R2quat(R)
+        # dq = q_final * inverse(q_init)
+        rot = tf.transformations.quaternion_multiply(rot, tf.transformations.quaternion_inverse(self.init_rot))
 
         return trans, rot
 
@@ -201,16 +177,6 @@ class VO:
         #print(trans)
 
         return trans, rot
-
-
-    def transformation_matrix(self, trans, R):
-        T = np.identity(4)
-        T[0:3, 0:3] = R
-        T[0,3] = trans[0]
-        T[1,3] = trans[1]
-        T[2,3] = trans[2]
-
-        return T
 
     def quat2R(self, quat):
         q0 = quat[3]
@@ -231,29 +197,23 @@ class VO:
         rot = [q1, q2, q3, q0]
         return rot
 
-
 if __name__ == "__main__":
     vo = VO()
     while not rospy.is_shutdown():
         try:
             if not vo.pose_is_init:
-                (vo.init_trans, vo.init_rot) = vo.listener.lookupTransform("/imu", "/ar_marker_1", rospy.Time(0))
+                (vo.init_trans, vo.init_rot) = vo.listener.lookupTransform("/ar_marker_1", "imu", rospy.Time(0))
                 vo.pose_is_init = True
                 print("pose is init")
-                #vo.broadcaster.sendTransform(vo.init_trans, vo.init_rot, rospy.Time(0),"/ar_marker_1", "/odom_vo")
-            
-            #vo.broadcaster.sendTransform(vo.init_trans, vo.init_rot, rospy.get_rostime(), "/ar_marker_1", "/odom_vo")
+                print(vo.init_trans)
 
             # tag fix
-            #(trans, rot) = vo.listener.lookupTransform("/cam_0_optical_frame", "/ar_marker_1", rospy.Time(0))
             (trans, rot) = vo.listener.lookupTransform( "/ar_marker_1", "/imu", rospy.Time(0))
-            #print(trans, rot)
 
-            #(trans, rot) = vo.relative_transformation(trans, rot)
-            #(trans, rot) = vo.inverse_transformation(trans, rot)
+            # get the relative transformation to the init pose
+            (trans, rot) = vo.relative_transformation(trans, rot)
 
-            #print(trans, rot)
-
+            # publish Visual Odometry message to ekf
             vo_msg = Odometry()
 
             vo_msg.header.frame_id = "/ar_marker_1"
@@ -263,6 +223,7 @@ if __name__ == "__main__":
             # vo_msg.pose.pose.position.x = -trans[0]
             # vo_msg.pose.pose.position.z = -trans[1]
             # vo_msg.pose.pose.position.y = trans[2]
+            relative_trans = [trans[0] - vo.init_trans[0],trans[1] - vo.init_trans[1],trans[2] - vo.init_trans[2]]
             vo_msg.pose.pose.position.x = trans[0]
             vo_msg.pose.pose.position.y = trans[1]
             vo_msg.pose.pose.position.z = trans[2]
@@ -270,6 +231,11 @@ if __name__ == "__main__":
             vo_msg.pose.pose.orientation.y = rot[1]
             vo_msg.pose.pose.orientation.z = rot[2]
             vo_msg.pose.pose.orientation.w = rot[3]
+
+            for i in range(0, 36, 7):
+                vo_msg.pose.covariance[i] = VO_SIGMA**2
+
+            vo.posepub.publish(vo_msg)
 
             if vo.start_vo:
                 # vo.x_vo.append(-trans[0])
@@ -286,21 +252,6 @@ if __name__ == "__main__":
 
                 vo.t_vo.append(rospy.Time.now().to_sec() - vo.vo_init_time)
 
-
-
-            # vo_msg.twist.twist.linear.x = self.state[6]
-            # vo_msg.twist.twist.linear.y = self.state[7]
-            # vo_msg.twist.twist.linear.z = self.state[8]
-            # vo_msg.twist.twist.angular.x = self.state[9]
-            # vo_msg.twist.twist.angular.y = self.state[10]
-            # vo_msg.twist.twist.angular.z = self.state[11]
-
-            for i in range(0, 36, 7):
-                vo_msg.pose.covariance[i] = VO_SIGMA**2
-            #vo_msg.twist.covariance = 
-
-            vo.posepub.publish(vo_msg)
-
         except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
             #print("no transformation")
             pass      
@@ -308,35 +259,44 @@ if __name__ == "__main__":
         vo.rate.sleep()
 
         if vo.finish:
+            # plot 3D path
             fig = plt.figure()
             ax = Axes3D(fig)
-            ax.plot(vo.x, vo.y, vo.z)
-            ax.plot(vo.x_vo, vo.y_vo, vo.z_vo)
-            #print(vo.x_vo, vo.y_vo, vo.z_vo)
+            ax.plot(vo.x, vo.y, vo.z, label="vio")
+            ax.plot(vo.x_vo, vo.y_vo, vo.z_vo, label="vo")
+            plt.legend()
 
+            # plot orientation
             plt.figure()
             plt.subplot(311)
-            plt.plot(vo.t, vo.roll)
-            plt.plot(vo.t_vo, vo.roll_vo)
-            #plt.plot(vo.t_imu, vo.roll_imu)
+            plt.plot(vo.t, vo.roll, label="vio")
+            plt.plot(vo.t_vo, vo.roll_vo, label="vo")
+            plt.plot(vo.t_imu, vo.roll_imu, label="imu")
+            plt.legend()
             plt.subplot(312)
-            plt.plot(vo.t, vo.pitch)
-            plt.plot(vo.t_vo, vo.pitch_vo)
-            #plt.plot(vo.t_imu, vo.pitch_imu)
+            plt.plot(vo.t, vo.pitch, label="vio")
+            plt.plot(vo.t_vo, vo.pitch_vo, label="vo")
+            plt.plot(vo.t_imu, vo.pitch_imu, label="imu")
+            plt.legend()
             plt.subplot(313)
-            plt.plot(vo.t, vo.yaw)
-            plt.plot(vo.t_vo, vo.yaw_vo)
-            #plt.plot(vo.t_imu, vo.yaw_imu)
+            plt.plot(vo.t, vo.yaw, label="vio")
+            plt.plot(vo.t_vo, vo.yaw_vo, label="vo")
+            plt.plot(vo.t_imu, vo.yaw_imu, label="imu")
+            plt.legend()
 
+            # plot position
             plt.figure()
             plt.subplot(311)
-            plt.plot(vo.t, vo.x)
-            plt.plot(vo.t_vo, vo.x_vo)
+            plt.plot(vo.t, vo.x, label="vio")
+            plt.plot(vo.t_vo, vo.x_vo, label="vo")
+            plt.legend()
             plt.subplot(312)
-            plt.plot(vo.t, vo.y)
-            plt.plot(vo.t_vo, vo.y_vo)
+            plt.plot(vo.t, vo.y, label="vio")
+            plt.plot(vo.t_vo, vo.y_vo, label="vo")
+            plt.legend()
             plt.subplot(313)
-            plt.plot(vo.t, vo.z)
-            plt.plot(vo.t_vo, vo.z_vo)
+            plt.plot(vo.t, vo.z, label="vio")
+            plt.plot(vo.t_vo, vo.z_vo, label="vo")
+            plt.legend()
             plt.show()
             break
